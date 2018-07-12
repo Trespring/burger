5654b984cb5867eb38829b58c6550ce7131145e0
Trespring <trespring@gmail.com>
2018-07-11 21:10:36 -0500

    added package.json

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('The Judge');
+INSERT INTO burgers (burger_name) VALUES ('Sauce Hog');
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Daisy Cuke', true);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('Uncle Messy', true);
+INSERT INTO burgers (burger_name, sleepy) VALUES ('The Luke', true);
+INSERT INTO burgers (burger_name) VALUES ('The Bo');
